1db832c7fd2e89596392543a065a64c15480ef61
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-07-16 05:39:38 +0100

    first commit

---
+++ global.js
@@ -0,0 +1,17 @@
+// console.log(global);
+
+// global.setTimeout(() => {
+//   console.log('in the timeout');
+// }, 3000);
+
+// setTimeout(() => {
+//   console.log('in the timeout');
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log('in the interval');
+// }, 1000);
+
+// console.log(__dirname); // gets the absolute path of the current folder
+// console.log(__filename); // gets the absolute path of the current folder with the filename added as well
