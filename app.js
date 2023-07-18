b056409377158b1a8c8a5fbbd2eda9260b088583
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-07-18 03:02:18 +0100

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -12,18 +13,22 @@ app.set('view engine', 'ejs');
 // listen for requests
 app.listen(3000);
 
-app.use((req, res, next) => {
-  console.log('new request made');
-  console.log('host:', req.hostname);
-  console.log('path:', req.path);
-  console.log('method:', req.method);
-  next();
-});
+// middleware & static files
+app.use(express.static('public')); // specifying public here makes all files in the public folder available to the browser so that's how we were able to use styles.
+app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  console.log('in the next middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('new request made');
+//   console.log('host:', req.hostname);
+//   console.log('path:', req.path);
+//   console.log('method:', req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('in the next middleware');
+//   next();
+// });
 
 // listen for the get request
 app.get('/', (req, res) => {
